d9252a11e573264151386c9642a1f0fdbf0e87cb
fdjkgh580 <fdjkgh580@yahoo.com.tw>
2017-08-28 15:50:42 +0800

    -

--- Codeigniter/Unit_test.php
+++ Codeigniter/Unit_test.php
@@ -1,15 +1,6 @@
 <?php 
 namespace Jsnlib\Codeigniter;
-/**
- * Codeigniter 中使用方式: 
- * 
- * $this->unit->run(......)
- * $this->unit->run(......)
- * $this->unit->run(......)
- * ......
- * $this->output->set_output(\Jsnlib\Codeigniter\Unit_test::report());
- * 
- */
+
 class Unit_test 
 {
     static protected $ci;
